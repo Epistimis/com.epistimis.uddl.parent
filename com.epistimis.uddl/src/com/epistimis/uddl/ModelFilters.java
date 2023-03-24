b8cf9ea3392bc8e5441ae9e3c0c49dccfef68a36
Steve Hickman <steve@epistimis.com>
2023-03-24 14:41:15 -0700

    LogicalMeasurement validation;

---
+++ com.epistimis.uddl/src/com/epistimis/uddl/ModelFilters.java
@@ -0,0 +1,27 @@
+package com.epistimis.uddl;
+
+import com.epistimis.uddl.uddl.ConceptualBasisEntity;
+import com.epistimis.uddl.uddl.LogicalEnumerated;
+import com.epistimis.uddl.uddl.LogicalEnumeratedBase;
+import com.epistimis.uddl.uddl.LogicalMeasurement;
+
+/**
+ * Containts methods used to filter model elements that are generally useful
+ * @author stevehickman
+ *
+ */
+public class ModelFilters {
+	
+
+
+	/**
+	 * T
+	 * @param lm The measurement in question
+	 * @param observableName The observable that should be associated with this measurement
+	 * @return true if the Measurement is associated with this Observable
+	 */
+	public static  boolean validMeasurement(LogicalMeasurement lm, String observableName) {
+		return lm.getRealizes().getName().equalsIgnoreCase(observableName);
+	}
+
+}
