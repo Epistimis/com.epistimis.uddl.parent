07c39e263bc144b7f7f108517e77748f7dabe7bd
Steve Hickman <steve@epistimis.com>
2023-07-15 16:20:45 -0700

    getObject(s)ForName added exceptions; containedIn handles partialMatch
    getAffectingElements added to TaxonomyProcessor

--- com.epistimis.uddl/src/com/epistimis/uddl/TaxonomyProcessor.java
+++ com.epistimis.uddl/src/com/epistimis/uddl/TaxonomyProcessor.java
@@ -3,49 +3,63 @@ package com.epistimis.uddl;
 import java.lang.reflect.ParameterizedType;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.eclipse.xtext.naming.QualifiedName;
 
+import com.epistimis.uddl.exceptions.NameCollisionException;
+import com.epistimis.uddl.exceptions.NamedObjectNotFoundException;
 import com.epistimis.uddl.scoping.IndexUtilities;
 import com.google.inject.Inject;
 
 /**
- * This generic base class is for when we want something more than an enum but less than a full class system. Taxonomies that
- * specialize this processor consist of four grammar rules. In Uddl.xtext, see these rules as an example:
- * LogicalEnumeratedBase:
-	LogicalEnumeratedSet |
-	LogicalEnumerationLabel |
-	LogicalEnumerated
+ * This generic base class is for when we want something more than an enum but
+ * less than a full class system. Taxonomies that specialize this processor
+ * consist of four grammar rules. In Uddl.xtext, see these rules as an example:
+ * LogicalEnumeratedBase: LogicalEnumeratedSet | LogicalEnumerationLabel |
+ * LogicalEnumerated
  * 
- * @param <Base> This is the type returned by the base rule (by default spelled the same as the base rule name)
+ * @param <Base> This is the type returned by the base rule (by default spelled
+ *               the same as the base rule name)
  */
 public abstract class TaxonomyProcessor<Base extends EObject> {
 	@Inject
 	IndexUtilities ndxUtil;
 
 	@Inject
-	IQualifiedNameProvider qnp;
+	UddlQNP qnp;
+
+	@Inject
+	IQualifiedNameConverter qnc;
 
 	public TaxonomyProcessor() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public static String DFLT_QN_DELIMITER = "\\.";
+
 	public enum TriBool {
 		FALSE, SOMETIMES, TRUE, UNNKNOWN
 	};
 
 	abstract public EClass getBaseMetaClass();
+
 	abstract public boolean isCastableToBase(EObject obj);
+
 	abstract public String getBaseName(EObject obj);
-	
+
 	/**
-	 * For generic classes, I sometimes need to know the type parameters. This general method
-	 * gets them for me
+	 * For generic classes, I sometimes need to know the type parameters. This
+	 * general method gets them for me
+	 * 
 	 * @param ndx The 0-based index of the type parameter I want
 	 * @return The Class instance of the type
 	 */
@@ -63,15 +77,15 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 	}
 
 	/**
-	 * From start, walk up containment hierarchy. This needs the 
+	 * From start, walk up containment hierarchy. This needs the
 	 * 
-	 * @param start The starting point in the taxonomy 
-	 * @param realType The base type of this taxonomy. Needed because
-	 * eContainment can continue walking up through unrelated types. We need to
-	 * stop when we exit this type.
+	 * @param start    The starting point in the taxonomy
+	 * @param realType The base type of this taxonomy. Needed because eContainment
+	 *                 can continue walking up through unrelated types. We need to
+	 *                 stop when we exit this type.
 	 * @return
 	 */
-	public  Collection<Base> collectAncestors(Base start) {
+	public Collection<Base> collectAncestors(Base start) {
 		Class realType = getBaseType();
 		List<Base> ancestors = new ArrayList<>();
 		EObject current = start;
@@ -86,10 +100,14 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 	/**
 	 * Get all the descendants of the starting point (including the starting point)
 	 * 
+	 * TODO: Do we need to check the contents to select only those of the correct
+	 * type? I don't think so since taxonomies contain only the correct type by
+	 * definition
+	 * 
 	 * @param start
 	 * @return
 	 */
-	public  Collection<Base> collectDescendants(Base start) {
+	public Collection<Base> collectDescendants(Base start) {
 		List<Base> descendants = new ArrayList<>();
 		descendants.add(start);
 		TreeIterator<EObject> titer = start.eAllContents();
@@ -102,56 +120,143 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 	}
 
 	/**
-	 * Is the test value anywhere in the ancestry of start. In other words,
-	 * is 'start' contained in the 'test' hierarchy?
+	 * When we want to check to see if an invariant affects something related to a
+	 * taxonomy, we need to check for invariants associated with the taxonomy member
+	 * under consideration. However, because taxonomies are defined & used
+	 * hierarchically, we also need to consider:
 	 * 
-	 * If this returns true, it also means that 'start' is a valid value for 
-	 * the 'test' label (if you treat 'test' as defining a set of values, then 'start'
+	 * A) The entire containment hierarchy for the specified element - because
+	 * 'this' element 'is-a' element for each of its containers in that taxonomy.
+	 * Because each taxonomy element specializes its containers, every invariant
+	 * that applies to one of its containers necessarily applies to it as well.
+	 * 
+	 * B) Its entire contents (everything contained within the element under
+	 * consideration) - because any code used for 'this' element could be used for
+	 * any of its specializing elements. Since invariants / constraints must be true
+	 * for the specified element, they must be true for all instances of that
+	 * element, which include all specializations of that element. A) differs from
+	 * B) in that A) is a guaranteed problem, whereas B) could be a problem.
+	 * 
+	 * This implies several things: 1) Each hierarchy should be constructed
+	 * assiduously. 2) Constraints/invariants/checks should be associated with an
+	 * element carefully
+	 * 
+	 * Without both of these, constraints can have unintended far reaching impact.
+	 * 
+	 * @param p
+	 * @return
+	 */
+	public Collection<Base> getAffectingElements(Base p) {
+
+		Collection<Base> results = collectAncestors(p);
+		results.addAll(collectDescendants(p));
+		return results;
+
+	}
+
+	/**
+	 * Is the test value anywhere in the ancestry of start. In other words, is
+	 * 'start' contained in the 'test' hierarchy?
+	 * 
+	 * If this returns true, it also means that 'start' is a valid value for the
+	 * 'test' label (if you treat 'test' as defining a set of values, then 'start'
 	 * can be one of them.
+	 * 
 	 * 
 	 * @param start
 	 * @param test
 	 * @return
 	 */
-	public  boolean containedIn(Base start, String test) {
+	public boolean containedIn(Base start, String test) {
 		Collection<Base> ancestors = collectAncestors(start);
 		return ancestors.stream().anyMatch(a -> getBaseName(a).equalsIgnoreCase(test));
 	}
 
 	/**
-	 * Standardized error message for use when 
-	 * the invalidValue is not in the taxonomic hierarchy starting at the container
+	 * Is the test value anywhere in the ancestry of start. In other words, is
+	 * 'start' contained in the 'test' hierarchy?
+	 * 
+	 * If this returns true, it also means that 'start' is a valid value for the
+	 * 'test' label (if you treat 'test' as defining a set of values, then 'start'
+	 * can be one of them.
+	 * 
+	 * NOTE: Because these are EObjects with names, we compare qualified names,
+	 * which may be relative
+	 * 
+	 * @param start
+	 * @param test
+	 * @return
+	 */
+	public boolean containedIn(Base start, String test, UddlQNP qnp) {
+		Collection<Base> ancestors = collectAncestors(start);
+		QualifiedName testName = qnc.toQualifiedName(test);
+		return ancestors.stream().anyMatch(a -> {
+			QualifiedName aqn = qnp.getFullyQualifiedName(a);
+			return qnp.partialMatch(aqn, testName);
+//			if (aqn == null) {
+//				// For some reason this didn't resolve properly? In any case, it can't match
+//				return false;
+//			}
+//			int diff = aqn.getSegmentCount() - testName.getSegmentCount();
+//			if (diff >= 0) {
+//				// The test name may be an RQN - so skip as needed on the aqn - but it could also be that the 
+//				// test name is higher in the taxonomy - or both
+//				boolean found = false;
+//				for (int i = 0; i < diff; i++) while (!found){
+//					if (aqn.getSegment(i).equalsIgnoreCase(testName.getFirstSegment())) {
+//						boolean partialFind = true;
+//						// We've found the start - keep going with the rest
+//						for (int j = i+1; j < testName.getSegmentCount(); j++) {
+//							if (!aqn.getSegment(j).equalsIgnoreCase(testName.getSegment(j-i))) {
+//								partialFind = false;
+//								break;
+//							}
+//						}
+//						found = partialFind;
+//					}
+//				}
+//				return found;
+//				//return aqn.skipFirst(diff).compareToIgnoreCase(testName) == 0;				
+//			} else {
+//				return false; // it can't possibly match
+//			}
+		});
+	}
+
+	/**
+	 * Standardized error message for use when the invalidValue is not in the
+	 * taxonomic hierarchy starting at the container
+	 * 
 	 * @param container
 	 * @param invalidValue
 	 */
 	public static String msgInvalidValue(String container, String invalidValue) {
-		String msg = MessageFormat.format("Container {0} contains an invalid value: {1}",
-				container, invalidValue);
+		String msg = MessageFormat.format("Container {0} contains an invalid value: {1}", container, invalidValue);
 		System.out.println(msg);
 		return msg;
 	}
 
 	/**
-	 * Look up the object with the specified name, as visible from the context instance
-	 * (In case we get a RQN and there is more than one)
+	 * Look up the object with the specified name, as visible from the context
+	 * instance (In case we get a RQN and there is more than one)
 	 * 
 	 * @param name
-	 * @return The found Object, or null if we can't find it or can't determine
-	 *         which it is. TODO: Should probably throw exceptions for failure here.
+	 * @return The found Object. If no object found, throws NamedObjectNotFoundException. Throws
+	 * 		NameCollisionException if multiple found with specified name (e.g. name is ambiguous)
 	 */
-	public Base getObjectForName(String name, EObject context) {
-
-		EObject inst = ndxUtil.getUniqueObjectForName(context, getBaseMetaClass(), name);
-		if (inst == null) {
+	public Base getObjectForName(String name, EObject context) throws NamedObjectNotFoundException, NameCollisionException {
+			
+			EObject inst = ndxUtil.getUniqueObjectForName(context, getBaseMetaClass(), name);
+			if (inst == null) {
+				return null;
+			}
+			if (isCastableToBase(inst)) {
+				return (Base) inst;
+			}
+	
+			/** If we get here, this isn't a purpose */
+			msgInvalidValue(name, inst.toString());
 			return null;
-		}
-		if (isCastableToBase(inst)) {
-			return (Base)inst;
-		}
-
-		/** If we get here, this isn't a purpose */
-		msgInvalidValue(name,inst.toString());
-		return null;
 	}
 
 	/**
@@ -159,45 +264,53 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 	 * the list itself may be empty.
 	 * 
 	 * @param names
-	 * @return a (possibly empty) list of objects.
+	 * @return a (possibly empty) list of objects. If no object found for a name, it is ignored. Throws
+	 * 		NameCollisionException if multiple found with specified name (e.g. name is ambiguous)
 	 */
-	public  List<Base> getObjectsForNames(List<String> names, EObject context) {
+	public List<Base> getObjectsForNames(List<String> names, EObject context)  throws NameCollisionException{
 		List<Base> result = new ArrayList<Base>();
 		for (String n : names) {
-			Base p = getObjectForName(n, context);
-			if (p != null) {
-				result.add(p);
+			try {
+				Base p = getObjectForName(n, context);
+				if (p != null) {
+					result.add(p);
+				}
+			} catch (NamedObjectNotFoundException excp) {
+				// Just ignore it
 			}
+
 		}
 		return result;
 	}
 
-
 	/**
-	 * When we want to check to see if an invariant affects something in a taxonomy, we need to check for 
-	 * invariants associated with the concept under consideration. However, when concepts 
-	 * can be defined & used hierarchically, we also need to consider:
+	 * When we want to check to see if an invariant affects something in a taxonomy,
+	 * we need to check for invariants associated with the concept under
+	 * consideration. However, when concepts can be defined & used hierarchically,
+	 * we also need to consider:
 	 * 
-	 * A) The entire containment hierarchy for the specified concept - because 'this' concept
-	 * 'is-a' concept for each of its containers. Because each concept specializes its containers,
-	 * every invariant that applies to one of its containers necessarily applies to it as well.
+	 * A) The entire containment hierarchy for the specified concept - because
+	 * 'this' concept 'is-a' concept for each of its containers. Because each
+	 * concept specializes its containers, every invariant that applies to one of
+	 * its containers necessarily applies to it as well.
 	 * 
-	 * B) Its entire contents (everything contained within the concept under consideration) - 
-	 * because any code used for 'this' concept could be used for any of its specializing Purposes. 
-	 * Since invariants / constraints must be true for the specified concept, they must be true for
-	 * all instances of that concept, which include all specializations of that concept.  A)
-	 * differs from B) in that A) is a guaranteed problem, whereas B) could be a problem.
+	 * B) Its entire contents (everything contained within the concept under
+	 * consideration) - because any code used for 'this' concept could be used for
+	 * any of its specializing Purposes. Since invariants / constraints must be true
+	 * for the specified concept, they must be true for all instances of that
+	 * concept, which include all specializations of that concept. A) differs from
+	 * B) in that A) is a guaranteed problem, whereas B) could be a problem.
 	 * 
-	 * This implies several things:
-	 * 1) The concept hierarchy should be constructed assiduously.
-	 * 2) Constraints/invariants/checks should be associated with concept carefully
+	 * This implies several things: 1) The concept hierarchy should be constructed
+	 * assiduously. 2) Constraints/invariants/checks should be associated with
+	 * concept carefully
 	 * 
 	 * Without both of these, constraints can have unintended far reaching impact.
 	 * 
 	 * @param p
 	 * @return
 	 */
-	public  List<Base> getAffectingConcepts(Base p) {
+	public List<Base> getAffectingConcepts(Base p) {
 		List<Base> results = new ArrayList<Base>();
 		results.add(p);
 		/**
@@ -205,31 +318,31 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 		 */
 		EObject current = (EObject) p;
 		while ((current.eContainer() != null) && (isCastableToBase(current.eContainer()))) {
-			current =  current.eContainer();
-			results.add((Base)current);
+			current = current.eContainer();
+			results.add((Base) current);
 		}
 		/**
 		 * We also get all the content of the original concept, if it is a set.
 		 * eAllContents returns a TreeIterator so we don't need to recurse separately
 		 */
-		for (EObject tp : IteratorExtensions.<EObject>toIterable(((EObject)p).eAllContents())) {
+		for (EObject tp : IteratorExtensions.<EObject>toIterable(((EObject) p).eAllContents())) {
 			if (isCastableToBase(tp)) {
 				results.add((Base) tp);
 			} else {
-				msgInvalidValue(qnp.getFullyQualifiedName(tp).toString(),tp.toString());			
+				msgInvalidValue(qnp.getFullyQualifiedName(tp).toString(), tp.toString());
 			}
 		}
 		return results;
 	}
 
 	/**
-	 * Check to see if the testedHierarchy overlaps with the hierarchyToCheck.
-	 * There are three possible results: 1) FALSE - the testedHierarchy is
-	 * completely unrelated to the hierarchyToCheck - this occurs when they have
-	 * no common ancestor or when they have a common ancestor but one is not the
-	 * ancestor of the other 2) UNNKNOWN - When TRUE and FALSE are not appropriate
-	 * 3) TRUE - if the testedHierarchy is wholly contained in the
-	 * hierarchyToCheck (includes when they are they same)
+	 * Check to see if the testedHierarchy overlaps with the hierarchyToCheck. There
+	 * are three possible results: 1) FALSE - the testedHierarchy is completely
+	 * unrelated to the hierarchyToCheck - this occurs when they have no common
+	 * ancestor or when they have a common ancestor but one is not the ancestor of
+	 * the other 2) UNNKNOWN - When TRUE and FALSE are not appropriate 3) TRUE - if
+	 * the testedHierarchy is wholly contained in the hierarchyToCheck (includes
+	 * when they are they same)
 	 * 
 	 * TODO: A strictly hierarchical definition of Hierarchies cannot handle
 	 * EU/Schengen area (https://en.wikipedia.org/wiki/Schengen_Area) - or anywhere
@@ -240,8 +353,7 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 	 * @param hierarchyToCheck
 	 * @return
 	 */
-	public TriBool intersectingHierarchies(Base testedHierarchy,
-			Base hierarchyToCheck) {
+	public TriBool intersectingHierarchies(Base testedHierarchy, Base hierarchyToCheck) {
 
 		/**
 		 * Quickly check to see if they are equal
@@ -258,8 +370,8 @@ public abstract class TaxonomyProcessor<Base extends EObject> {
 			return TriBool.TRUE;
 
 		/**
-		 * If the testedHierarchy is an ancestor of the hierarchyToCheck, then
-		 * MAYBE (because we might be dealing with something in the hierarchyToCheck)
+		 * If the testedHierarchy is an ancestor of the hierarchyToCheck, then MAYBE
+		 * (because we might be dealing with something in the hierarchyToCheck)
 		 */
 		Collection<Base> hTCAncestors = collectAncestors(hierarchyToCheck);
 		if (hTCAncestors.contains(testedHierarchy))
