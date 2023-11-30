7a8c5ac6ca5c5cb4b0cc5110d271f22ff349916d
Steve Hickman <steve@epistimis.com>
2023-11-30 14:52:42 -0800

    Add Web

--- resig.sh
+++ resig.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 git restore  com.epistimis.uddl.ide/src-gen/.gitignore
 git restore com.epistimis.uddl.ide/xtend-gen/.gitignore
+git restore com.epistimis.uddl.web/src-gen/.gitignore
+git restore com.epistimis.uddl.web/xtend-gen/.gitignore
 git restore com.epistimis.uddl.ui.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.ui/src-gen/.gitignore
 git restore com.epistimis.uddl/src-gen/.gitignore
