8c4ef1c3d06874d64cc9919e0163a6c9fc5f7e45
Steve Hickman <steve@epistimis.com>
2023-11-30 17:40:59 -0800

    Fix spelling error that missed a file

--- resig.sh
+++ resig.sh
@@ -2,7 +2,7 @@
 git restore  com.epistimis.uddl.ide/src-gen/.gitignore
 git restore com.epistimis.uddl.ide/xtend-gen/.gitignore
 git restore com.epistimis.uddl.tests/src-gen/.gitignore
-git restore com.epistimis.uddl.tests/xted-gen/.gitignore
+git restore com.epistimis.uddl.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.web/src-gen/.gitignore
 git restore com.epistimis.uddl.web/xtend-gen/.gitignore
 git restore com.epistimis.uddl.ui.tests/src-gen/.gitignore
