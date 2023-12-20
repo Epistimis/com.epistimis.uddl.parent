c43e881bf2b3ada0d51be04eb352a21288361e49
Steve Hickman <steve@epistimis.com>
2023-12-20 11:57:34 -0800

    isTypeOf, typeHierarchyRoot, exactlyOneRoot, containsDataFromType,
    Also, for ConceptualEntity:
    typeUsesType, checkASetOfTypes, addlReferencedModelTypes,
    checkAType4Refs, specializationAncestry,

--- resig.sh
+++ resig.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-git restore  com.epistimis.uddl.ide/src-gen/.gitignore
+git restore com.epistimis.uddl.ide/src-gen/.gitignore
 git restore com.epistimis.uddl.ide/xtend-gen/.gitignore
 git restore com.epistimis.uddl.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.tests/xtend-gen/.gitignore
@@ -8,5 +8,6 @@ git restore com.epistimis.uddl.web/xtend-gen/.gitignore
 git restore com.epistimis.uddl.ui.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.ui/src-gen/.gitignore
 git restore com.epistimis.uddl/src-gen/.gitignore
+git restore com.epistimis.uddl/xtend-gen/.gitignore
 git status
 
