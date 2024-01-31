c61db2ce52b4a9a889674ba778b45e189126bdbb
Steve Hickman <steve@epistimis.com>
2024-01-31 12:53:19 -0800

    Flush Realization caches before resolving to avoid memory leaks

--- com.epistimis.uddl/src/com/epistimis/uddl/RealizationResolver.java
+++ com.epistimis.uddl/src/com/epistimis/uddl/RealizationResolver.java
@@ -28,56 +28,75 @@ public class RealizationResolver {
 
 	private static Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass());
 
-	@Inject IQualifiedNameProvider qnp;
-	
+	@Inject
+	IQualifiedNameProvider qnp;
+
 	/**
-	 * 'Realize' and resolve all the EObjects in the specified resource. The results are cached.
+	 * 'Realize' and resolve all the EObjects in the specified resource. The results
+	 * are cached.
+	 * 
 	 * @param resource
 	 */
 	public static void resolve(Resource resource) {
-		
+
 		ResourceSet rs = resource.getResourceSet();
 		resolve(rs);
 	}
 
 	/**
-	 * 'Realize' and resolve all the EObjects in the specified ResourceSet. The results are cached.
+	 * 'Realize' and resolve all the EObjects in the specified ResourceSet. The
+	 * results are cached.
+	 * 
 	 * @param resource
 	 */
 	public static void resolve(ResourceSet rs) {
-		
+
 		/**
-		 * Before doing anything else, resolve all ECore cross references - because we don't know if some have been lazy linked
+		 * Before doing anything else, resolve all ECore cross references - because we
+		 * don't know if some have been lazy linked
 		 */
-		//EcoreUtil2.resolveAll(rs);
-		
-		for (Resource res: rs.getResources()) {			
+		// EcoreUtil2.resolveAll(rs);
+
+		// Before creating new instances, check to see if we already have an instance.
+		// TODO: We need a way to determine if the AST has changed so we can invalidate the cache.
+		// How do we do that? Since the cache is keyed by instance ID, it is sufficient that a change
+		// will cause a new instance to be created, so the old instance won't be found? The problem
+		// is that approach leaves the old instances in the cache, just unused - which creates a memory
+		// leak. We need to know to remove old instances. Alternatively, we just always flush the cache
+		// at this point - which is what we do here.
+		RealizedComposableElement.allComposable2Realized.clear();
+		RealizedComposableElement.allRealized2Composable.clear();
+
+		for (Resource res : rs.getResources()) {
 			/**
-			 * This will only collect all the realization
-			 * info from a PlatformEntity -> LogicalEntity -> ConceptualEntity to determine what is available. The results can then be used
-			 * to create instances, generate code, or ....
+			 * This will only collect all the realization info from a PlatformEntity ->
+			 * LogicalEntity -> ConceptualEntity to determine what is available. The results
+			 * can then be used to create instances, generate code, or ....
 			 */
-			    final Iterable<PlatformComposableElement> elements = Iterables.<PlatformComposableElement>filter(IteratorExtensions.<EObject>toIterable(res.getAllContents()), PlatformComposableElement.class);
-			    for (final PlatformComposableElement elem : elements) {
-			      if ((elem instanceof PlatformAssociation)) {
-			        new RealizedAssociation(((PlatformAssociation)elem));
-			      } else {
-			        if ((elem instanceof PlatformDataType)) {
-			          new RealizedDataType(((PlatformDataType)elem));
-			        } else {
-			          if ((elem instanceof PlatformEntity)) {
-			            new RealizedEntity(((PlatformEntity)elem));
-			          } else {
-			            logger.warn(MessageFormat.format("No processing available for type {0}",elem.getClass().toString()));
-			          }
-			        }
-			      }
-			    }
+			final Iterable<PlatformComposableElement> elements = Iterables.<PlatformComposableElement>filter(
+					IteratorExtensions.<EObject>toIterable(res.getAllContents()), PlatformComposableElement.class);
+				
+			for (final PlatformComposableElement elem : elements) {
+				if ((elem instanceof PlatformAssociation)) {
+					new RealizedAssociation(((PlatformAssociation) elem));
+				} else {
+					if ((elem instanceof PlatformDataType)) {
+						new RealizedDataType(((PlatformDataType) elem));
+					} else {
+						if ((elem instanceof PlatformEntity)) {
+							new RealizedEntity(((PlatformEntity) elem));
+						} else {
+							logger.warn(MessageFormat.format("No processing available for type {0}",
+									elem.getClass().toString()));
+						}
+					}
+				}
+			}
 		}
 		/**
 		 * Now go back and link all the PlatformEntity types
 		 * 
 		 */
-	    RealizedComposableElement.linkTypes();
+		RealizedComposableElement.linkTypes();
 	}
 }
